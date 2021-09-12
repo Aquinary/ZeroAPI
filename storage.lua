88e9747c7ec54056b941d2d0445f4cdcb93b691f
Aquinary <aquinary@yandex.ru>
2021-09-13 01:06:04 +0300

    Run-storage base functional

--- storage.lua
+++ storage.lua
@@ -4,11 +4,12 @@ function Z:Storage()
 
     --[[
         Постоянное хранилище, которое работает до тех пор, пока не будет выключен мод.
-        Перезапуск игры: данные сохраняются
-        Начало нового забега: данные сохраняются
+
+        Новый забег: данные сохраняются
+        Перезапуск игры (продолжение забега): данные сохраняются
     --]]
     obj.persistent = {
-        -- Установка нового значения
+        -- Установка значения
         set = function(key, value)
             ModSettingSet(key, value)
         end,
@@ -24,6 +25,59 @@ function Z:Storage()
         -- Удаление значения
         remove = function(key)
             ModSettingSet(key, nil)
+        end,
+    }
+
+    --[[
+        Хранилище, используемое в рамках запуска одной игрового забега
+
+        Новый забег: данные не сохраняются
+        Перезапуск игры (продолжение забега): данные сохраняются
+    --]]
+    obj.run = {
+        -- Установка значения
+        set = function(key, value)
+            GlobalsSetValue(key, value)
+        end,
+        -- Получение значения
+        get = function(key)
+            local _val = GlobalsGetValue(key)
+            return _val ~= '' and _val or nil
+        end,
+        -- Проверка ключа на nil
+        has = function(key)
+            local _val = GlobalsGetValue(key)
+            return _val ~= '' and true or false
+        end,
+        -- Удаление значения
+        remove = function(key)
+            GlobalsSetValue(key, '')
+        end,
+    }
+
+    --[[
+        Хранилище, используемое в рамках запуска одной игровой сессии
+
+        Новый забег: данные не сохраняются
+        Перезапуск игры (продолжение забега): данные не сохраняются
+    --]]
+    obj.session = {
+        -- Установка значения
+        set = function(key, value)
+
+        end,
+        -- Получение значения
+        get = function(key)
+            return nil
+        end,
+        -- Проверка ключа на nil
+        has = function(key)
+            local _val = nil
+            return _val ~= nil and true or false
+        end,
+        -- Удаление значения
+        remove = function(key)
+
         end,
     }
 
